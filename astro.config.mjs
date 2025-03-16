c4a90c77ab9df64a3931e66291cd061dd3023a8f
Felix <felixzitzelsberger@yahoo.com>
2025-03-16 10:28:06 +0100

    Installed and added nodejs server adapter to astro config, so it will work in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 // Make sure to uncomment the base when publishing to GITHUB PAGES
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://capitanski.github.io/',
-    outDir: './docs',
-    output: 'server',
-    //base: 'matuschek',
-    build: {
-        assets: 'custom-folder'
+  site: "https://capitanski.github.io/",
+  outDir: "./docs",
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  //base: 'matuschek',
+  build: {
+    assets: "custom-folder",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
     },
-    vite: {
-        resolve: {
-            alias: {
-                "@": "/src"
-            }
-        }
-    }
+  },
 });
