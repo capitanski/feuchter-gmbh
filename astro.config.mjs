62cd94613bacc1ea1726fba6b7eaa78f4cf18a4d
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-03-18 17:45:10 +0100

    Projekte Header Bild eingefügt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+//import react from "@astrojs/react";
 // Make sure to uncomment the base when publishing to GITHUB PAGES
 // https://astro.build/config
 export default defineConfig({
   site: "https://capitanski.github.io/",
+  //integrations: [react()],
   outDir: "./docs",
   output: "server",
   adapter: node({ mode: "standalone" }),
