b399497e2b1e360f89e05dbcb4bf7b6fc4aebb46
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-03-20 16:15:56 +0100

    Translation Funktion erweitert und mit kommentaren versehen

--- src/content/translations.js
+++ src/content/translations.js
@@ -1,3 +1,5 @@
+// Jedes zu übersetzende Element sollte in seiner Definition (Quelle alias JS Object) einen Translationkey haben
+// Oder die Translations direkt im Quell Objekt definieren, dann kann man sich diese Funktion hier sparen
 export function translations(key, lang) {
     let response;
     switch (key) {
@@ -18,6 +20,56 @@ export function translations(key, lang) {
                     break;
                 case "de":
                     response = "Stück";
+                    break;
+            }
+            break;
+        case "leistungen":
+            switch (lang) {
+                case "en":
+                    response = "Services";
+                    break;
+                case "de":
+                    response = "Leistungen";
+                    break;
+            }
+            break;
+        case "leistungensubtitle":
+            switch (lang) {
+                case "en":
+                    response = "From development to production and logistics";
+                    break;
+                case "de":
+                    response = "Von der Entwicklung, über die Produktion bis hin zur Logistik"
+                    break;
+            }
+            break;
+        case "konzeption":
+            switch (lang) {
+                case "en":
+                    response = "Development and Conception";
+                    break;
+                case "de":
+                    response = "Entwicklung & Konzeption";
+                    break;
+            }
+            break;
+        case "produktion":
+            switch (lang) {
+                case "en":
+                    response = "Production";
+                    break;
+                case "de":
+                    response = "Produktion";
+                    break;
+            }
+            break;
+        case "logistik":
+            switch (lang) {
+                case "en":
+                    response = "Logistics";
+                    break;
+                case "de":
+                    response = "Logistik & Vertrieb";
                     break;
             }
             break;
