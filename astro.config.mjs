c1dcced9804e10f28325e023ad03b38b486f49b2
Felix <felixzitzelsberger@yahoo.com>
2025-03-20 08:30:53 +0100

    mdx Datei angelegt und mdx Plugin installiert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import mdx from "@astrojs/mdx";
 //import react from "@astrojs/react";
 // Make sure to uncomment the base when publishing to GITHUB PAGES
 // https://astro.build/config
 export default defineConfig({
   site: "https://capitanski.github.io/",
-  //integrations: [react()],
+  integrations: [mdx()],
   outDir: "./docs",
   output: "server",
   adapter: node({ mode: "standalone" }),
