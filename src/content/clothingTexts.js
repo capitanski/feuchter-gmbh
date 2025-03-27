103f4403940ae5dccc13c8543d3205f3fa08c6ff
Felix <felixzitzelsberger@yahoo.com>
2025-03-27 07:22:22 +0100

    Arrow für technische Produktdaten neu gemacht, produktliste neu gestyled

--- src/content/clothingTexts.js
+++ src/content/clothingTexts.js
@@ -1,63 +1,100 @@
 import schutzwand from "@/assets/projekte.jpg";
 import thermohaube from "@/assets/officialwear_header.jpg";
 import staubhaube from "@/assets/corporate_fashion.jpg";
+import mobileSchutzwand from "@/assets/Transportwand mobile.jpg";
 export const clothingTexts = {
-    militarysurvival: {
-        heading: {
-            de: "High-End Militär- & Survivalkleidung vom Traditionshersteller",
-            en: "High-end military and survival clothing from a traditional manufacturer."
-        },
-        subtitle: {
-            de: "Produziert nach Original-TL",
-            en: "Manufactured according to original technical specifications"
-        },
-        introText: {
-            de: "Von robuster Militär- und Survivalkleidung über Einsatzanzüge bis hin zum Rucksacküberzug – wir statten Sie aus. Ob für Armeen, Einsatzkräfte, Onlineshops oder Unternehmen: Unsere Produkte vereinen Qualität, Funktionalität und Design auf höchstem Niveau. Vertrauen Sie auf Erfahrung, Präzision und textile Lösungen, die den Unterschied machen. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
-            en: "From rugged military and survival clothing to tactical suits and backpack covers – we’ve got you covered. Whether for armed forces, emergency services, online shops, or businesses: our products combine quality, functionality, and design at the highest level. Rely on experience, precision, and textile solutions that make a difference. Feel free to contact us for inquiries or custom solutions."
-        },
-        suitableFor: {
-            de: ["Armeen", "Onlineshops", "Outdoorausstatter", "Jagdshops", "Anglershops"],
-            en: ["Armed forces", "Online shops", "Outdoor outfitters", "Hunting shops", "Fishing shops"]
-
-        },
-        headerImage: schutzwand.src
+  militarysurvival: {
+    heading: {
+      de: "High-End Militär- & Survivalkleidung vom Traditionshersteller",
+      en: "High-end military and survival clothing from a traditional manufacturer.",
     },
-    officialwear: {
-        heading: {
-            de: "Behörden- & Securitykleidung",
-            en: "Officialwear and Securityclothing"
-        },
-        subtitle: {
-            de: "Transportieren und Lagern Sie Ihre Waren sicher und wohltemperiert von A nach B",
-            en: "Transport and store your goods safely and at the right temperature from A to B"
-        },
-        introText: {
-            de: "Wir fertigen hochwertige Thermohauben in verschiedenen Maßen für Rollcontainer. Unsere innovativen Lösungen ersetzen herkömmliche Stretchfolie vollständig und bieten eine nachhaltige, wiederverwendbare Alternative zur Ladungssicherung & Isolierung. Unsere Produkte sorgen für zuverlässige und umweltfreundliche Abdeckung. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
-            en: "We manufacture high-quality thermal covers in various sizes for roll containers. Our innovative solutions completely replace conventional stretch film and offer a sustainable, reusable alternative for load securing and insulation. Our products ensure reliable and environmentally friendly coverage. Feel free to contact us for inquiries or custom solutions."
-        },
-        suitableFor: {
-            de: ["Cateringunternehmen", "Supermarktketten", "Konditoreien", "Gastronomie"],
-            en: ["Catering companies", "Supermarket chains", "Pastry shops", "Hospitality industry"]
-        },
-        headerImage: thermohaube.src
+    subtitle: {
+      de: "Produziert nach Original-TL",
+      en: "Manufactured according to original technical specifications",
     },
-    corporatefashion: {
-        heading: {
-            de: "Corporate Fashion für Unternehmen",
-            en: "Corporate Fashion for Businesses"
-        },
-        subtitle: {
-            de: "Individuell abgestimmt auf Ihre Bedürfnisse",
-            en: "Transport and store your goods safely and out of sight from A to B."
-        },
-        introText: {
-            de: "Wir fertigen hochwertige Staubhauben in verschiedenen Maßen für Rollcontainer. Unsere innovativen Lösungen ersetzen herkömmliche Stretchfolie vollständig und bieten eine nachhaltige, wiederverwendbare Alternative zur Ladungssicherung. Unsere Produkte sorgen für zuverlässige und umweltfreundliche Abdeckung. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
-            en: "We manufacture high-quality dust covers in various sizes for roll containers. Our innovative solutions completely replace conventional stretch film and offer a sustainable, reusable alternative for load securing. Our products ensure reliable and environmentally friendly coverage. Feel free to contact us for inquiries or custom solutions."
-        },
-        suitableFor: {
-            de: ["Cateringunternehmen", "Supermarktketten", "Konditoreien", "Gastronomie", "Logistikunternehmen"],
-            en: ["Catering companies", "Supermarket chains", "Pastry shops", "Hospitality industry", "Logistics Companies"]
-        },
-        headerImage: staubhaube.src
-    }
-}
+    introText: {
+      de: "Von robuster Militär- und Survivalkleidung über Einsatzanzüge bis hin zum Rucksacküberzug – wir statten Sie aus. Ob für Armeen, Einsatzkräfte, Onlineshops oder Unternehmen: Unsere Produkte vereinen Qualität, Funktionalität und Design auf höchstem Niveau. Vertrauen Sie auf Erfahrung, Präzision und textile Lösungen, die den Unterschied machen. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
+      en: "From rugged military and survival clothing to tactical suits and backpack covers – we’ve got you covered. Whether for armed forces, emergency services, online shops, or businesses: our products combine quality, functionality, and design at the highest level. Rely on experience, precision, and textile solutions that make a difference. Feel free to contact us for inquiries or custom solutions.",
+    },
+    suitableFor: {
+      de: [
+        "Armeen",
+        "Onlineshops",
+        "Outdoorausstatter",
+        "Jagdshops",
+        "Anglershops",
+      ],
+      en: [
+        "Armed forces",
+        "Online shops",
+        "Outdoor outfitters",
+        "Hunting shops",
+        "Fishing shops",
+      ],
+    },
+    headerImage: schutzwand.src,
+    mobileHeaderImage: mobileSchutzwand.src,
+  },
+  officialwear: {
+    heading: {
+      de: "Behörden- & Securitykleidung",
+      en: "Officialwear and Securityclothing",
+    },
+    subtitle: {
+      de: "Transportieren und Lagern Sie Ihre Waren sicher und wohltemperiert von A nach B",
+      en: "Transport and store your goods safely and at the right temperature from A to B",
+    },
+    introText: {
+      de: "Wir fertigen hochwertige Thermohauben in verschiedenen Maßen für Rollcontainer. Unsere innovativen Lösungen ersetzen herkömmliche Stretchfolie vollständig und bieten eine nachhaltige, wiederverwendbare Alternative zur Ladungssicherung & Isolierung. Unsere Produkte sorgen für zuverlässige und umweltfreundliche Abdeckung. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
+      en: "We manufacture high-quality thermal covers in various sizes for roll containers. Our innovative solutions completely replace conventional stretch film and offer a sustainable, reusable alternative for load securing and insulation. Our products ensure reliable and environmentally friendly coverage. Feel free to contact us for inquiries or custom solutions.",
+    },
+    suitableFor: {
+      de: [
+        "Cateringunternehmen",
+        "Supermarktketten",
+        "Konditoreien",
+        "Gastronomie",
+      ],
+      en: [
+        "Catering companies",
+        "Supermarket chains",
+        "Pastry shops",
+        "Hospitality industry",
+      ],
+    },
+    headerImage: thermohaube.src,
+    mobileHeaderImage: mobileSchutzwand.src,
+  },
+  corporatefashion: {
+    heading: {
+      de: "Corporate Fashion für Unternehmen",
+      en: "Corporate Fashion for Businesses",
+    },
+    subtitle: {
+      de: "Individuell abgestimmt auf Ihre Bedürfnisse",
+      en: "Transport and store your goods safely and out of sight from A to B.",
+    },
+    introText: {
+      de: "Wir fertigen hochwertige Staubhauben in verschiedenen Maßen für Rollcontainer. Unsere innovativen Lösungen ersetzen herkömmliche Stretchfolie vollständig und bieten eine nachhaltige, wiederverwendbare Alternative zur Ladungssicherung. Unsere Produkte sorgen für zuverlässige und umweltfreundliche Abdeckung. Kontaktieren Sie uns gerne für Anfragen oder individuelle Lösungen.",
+      en: "We manufacture high-quality dust covers in various sizes for roll containers. Our innovative solutions completely replace conventional stretch film and offer a sustainable, reusable alternative for load securing. Our products ensure reliable and environmentally friendly coverage. Feel free to contact us for inquiries or custom solutions.",
+    },
+    suitableFor: {
+      de: [
+        "Cateringunternehmen",
+        "Supermarktketten",
+        "Konditoreien",
+        "Gastronomie",
+        "Logistikunternehmen",
+      ],
+      en: [
+        "Catering companies",
+        "Supermarket chains",
+        "Pastry shops",
+        "Hospitality industry",
+        "Logistics Companies",
+      ],
+    },
+    headerImage: staubhaube.src,
+    mobileHeaderImage: mobileSchutzwand.src,
+  },
+};
