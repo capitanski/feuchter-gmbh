ee90999c63990cd7a9c966d1127b7dad9b78cefd
Felix <felixzitzelsberger@yahoo.com>
2025-03-30 12:09:51 +0200

    Produktmodel in content/productmodel angelegt, auf dieser Basis können jetzt Produkte angelegt werden

---
+++ src/content/productmodel/productModel.js
@@ -0,0 +1,44 @@
+const products = [];
+const product = {
+  title: {
+    de: "Produkttitel",
+    en: "Producttitle",
+  },
+  shortDescription: {
+    de: "Kurzbeschreibung",
+    en: "Short Description",
+  },
+  category: {
+    de: "Hemden/Schutzwand etc",
+    en: "Shirts/Protective Walls etc",
+  },
+  tag: {
+    de: "Original TL/Individualisierbar",
+    en: "Original TL/Customizable",
+  },
+  images: ["imageurl", "imageurl", "imageurl"],
+  lists: [
+    {
+      title: {
+        de: "Listentitel",
+        en: "List title",
+      },
+      content: [
+        {
+          de: "Listenpunkt",
+          en: "Bulletpoint",
+        },
+      ],
+    },
+  ],
+  optionalRichtext: {
+    title: {
+      de: "Richtext Titel Optional",
+      en: "Richtext title optional",
+    },
+    content: {
+      de: "Deutscher Richtext set:html",
+      en: "English Richtext set:html",
+    },
+  },
+};
