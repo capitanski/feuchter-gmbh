f5a511b88c5b4cf6baa41d1fb63548b2b7170adf
Capitanski <felixzitzelsberger@yahoo.com>
2025-03-31 15:50:17 +0200

    Ubuntu build

--- dist/server/pages/_lang_/contact.astro.mjs
+++ dist/server/pages/_lang_/contact.astro.mjs
@@ -1,7 +1,7 @@
 import { e as createAstro, f as createComponent, m as maybeRenderHead, h as addAttribute, i as renderComponent, r as renderTemplate } from '../../chunks/astro/server_CofuBfMf.mjs';
 import 'kleur/colors';
 import { C as Circles, $ as $$Layout } from '../../chunks/Layout_CdeovOSY.mjs';
-import { $ as $$ContactForm } from '../../chunks/ContactForm_CTGokQIw.mjs';
+import { $ as $$ContactForm } from '../../chunks/ContactForm_Cjw6quwO.mjs';
 /* empty css                                      */
 export { renderers } from '../../renderers.mjs';
 
@@ -10,7 +10,7 @@ const $$WelcomeContact = createComponent(($$result, $$props, $$slots) => {
   const Astro2 = $$result.createAstro($$Astro$1, $$props, $$slots);
   Astro2.self = $$WelcomeContact;
   const { language, heading, subtitle, introText, Scribble, page } = Astro2.props;
-  return renderTemplate`${maybeRenderHead()}<section data-astro-cid-eu5miymy> <img${addAttribute(Scribble.src, "src")} alt="" class="scribble" fetchpriority="high" loading="eager" data-astro-cid-eu5miymy> <div class="container" data-astro-cid-eu5miymy> <img${addAttribute(Circles.src, "src")} alt="" class="circles-svg" fetchpriority="high" loading="eager" data-astro-cid-eu5miymy> <div class="hero-container" data-astro-cid-eu5miymy> ${renderComponent($$result, "ContactForm", $$ContactForm, { "data-astro-cid-eu5miymy": true })} </div> </div> </section> `;
+  return renderTemplate`${maybeRenderHead()}<section data-astro-cid-eu5miymy> <img${addAttribute(Scribble.src, "src")} alt="" class="scribble" fetchpriority="high" loading="eager" data-astro-cid-eu5miymy> <div class="container" data-astro-cid-eu5miymy> <img${addAttribute(Circles.src, "src")} alt="" class="circles-svg" fetchpriority="high" loading="eager" data-astro-cid-eu5miymy> <div class="hero-container" data-astro-cid-eu5miymy> ${renderComponent($$result, "ContactForm", $$ContactForm, { "heading": heading, "subtitle": subtitle, "data-astro-cid-eu5miymy": true })} </div> </div> </section> `;
 }, "/home/felix/Desktop/feuchtergmbh_website/feuchter-gmbh/src/components/WelcomeContact.astro", void 0);
 
 const Scribble = new Proxy({"src":"/custom-folder/teamimage.SGbNsW24.jpg","width":3000,"height":1000,"format":"jpg"}, {
@@ -37,8 +37,8 @@ const $$Contact = createComponent(($$result, $$props, $$slots) => {
   let introText = `
 Wir unterst\xFCtzen unsere Kunden von Anfang an bei ihren Projekten \u2013 von der Konzeption und Entwicklung \xFCber die Produktion bis hin zur Logistik. Mit unserer Expertise sorgen wir f\xFCr ma\xDFgeschneiderte L\xF6sungen, die effizient, zuverl\xE4ssig und nachhaltig sind.`;
   if (pageLanguage == "en") {
-    heading = "We are looking forward to hear from you";
-    subtitle = "From conception to production to logistics";
+    heading = "Your Vision is just a click away";
+    subtitle = "We are looking forward to hear from you";
     introText = `
 We support our customers from the very beginning of their projects \u2013 from conception and development to production and logistics. With our expertise, we ensure tailored solutions that are efficient, reliable, and sustainable.`;
   }
@@ -49,10 +49,10 @@ const $$file = "/home/felix/Desktop/feuchtergmbh_website/feuchter-gmbh/src/pages
 const $$url = "/[lang]/contact";
 
 const _page = /*#__PURE__*/Object.freeze(/*#__PURE__*/Object.defineProperty({
-    __proto__: null,
-    default: $$Contact,
-    file: $$file,
-    url: $$url
+  __proto__: null,
+  default: $$Contact,
+  file: $$file,
+  url: $$url
 }, Symbol.toStringTag, { value: 'Module' }));
 
 const page = () => _page;
