d10b864fc14fb774c33934368f22dfcc17363428
Felix <felixzitzelsberger@yahoo.com>
2025-03-31 08:02:23 +0200

    Produktmodel angelegt und mit Testartikel befüllt

--- src/content/productmodel/productModel.js
+++ src/content/productmodel/productModel.js
@@ -1,32 +1,71 @@
 const products = [];
 const product = {
   title: {
-    de: "Produkttitel",
-    en: "Producttitle",
+    de: "Feldhose Tropentarn",
+    en: "Desert camo field trousers",
   },
   shortDescription: {
-    de: "Kurzbeschreibung",
-    en: "Short Description",
+    de: `Die Feldhose, Tarndruck ist Teil des Feldanzuges der BW. Sie wird in den Varianten als 5 Farben-Tarndruck ohne und mit Vektorenschutzausrüstung gefertigt. Hose mit angeschnittenem Bund und Knopfverschluss, Reißverschluss im Schlitz, Bundfalten, 2 Flügeltaschen, 2 aufgesetzte Blasebalgtaschen mit Patten und Druckknopfverschluss, 1 Gesäß-Pattentasche mit Knopf und Knopfloch (rechtes Hinterhosenteil), 1 Klappmessertasche, 8 Bundschlaufen. Material Tarn: 65% Baumwolle, 35% Polyester`,
+    en: `The camouflage field trousers are part of the German Bundeswehr (BW) uniform. They are produced in two versions: a 5-color camouflage print, with and without vector protection equipment.
+
+    The trousers feature an integrated waistband with button closure, zipper fly, pleats at the waist, two side slash pockets, two patch bellows pockets with flaps and snap button closures, one rear flap pocket with button and buttonhole (on the right back panel), one pocket for a folding knife, and eight belt loops.
+    
+    Camouflage material: 65% cotton, 35% polyester.`,
   },
   category: {
-    de: "Hemden/Schutzwand etc",
-    en: "Shirts/Protective Walls etc",
+    de: "Hosen",
+    en: "Pants",
   },
   tag: {
-    de: "Original TL/Individualisierbar",
-    en: "Original TL/Customizable",
+    de: "Original TL",
+    en: "Original TL",
   },
-  images: ["imageurl", "imageurl", "imageurl"],
+  images: [
+    "https://cdn.prod.website-files.com/66fbdc612b6ec708353a8c01/67ceff6e127b1c203bb4b3c9_Tropentarn%20Feldhose%20Front.jpg",
+    "https://cdn.prod.website-files.com/66fbdc612b6ec708353a8c01/67ceff785843abeb9d4dfe04_Tropentarn%20Feldhose%20Seite.jpg",
+  ],
   lists: [
     {
       title: {
-        de: "Listentitel",
-        en: "List title",
+        de: "Technische Daten",
+        en: "Technical Data",
       },
       content: [
         {
-          de: "Listenpunkt",
-          en: "Bulletpoint",
+          de: "gerade und bequem geschnittene Feldhose",
+          en: "Straight-cut and comfortably fitting field trousers",
+        },
+        {
+          de: "Original nach Bundeswehr TL",
+          en: "Original according to Bundeswehr technical specifications (TL)",
+        },
+        {
+          de: "2 Einschubtaschen",
+          en: "2 slash pockets",
+        },
+        {
+          de: "2 aufgesetzte Beintaschen mit Patte und Druckknopfverschluss",
+          en: "2 patch cargo pockets with flaps and snap button closures",
+        },
+        {
+          de: "1 Gesäßtasche mit Patte und Knopfverschluss",
+          en: "1 rear pocket with flap and button closure",
+        },
+        {
+          de: "umlaufend 4 cm breite Gürtelschlaufen",
+          en: "4 cm wide belt loops all around",
+        },
+        {
+          de: "Verschluss mit Reißverschluss und Knopf",
+          en: "Closure with zipper and button",
+        },
+        {
+          de: "Knöpfe am Bund zum Anbringen von Hosenträgern",
+          en: "Buttons on the waistband for attaching suspenders",
+        },
+        {
+          de: "besonders robustes Gewebe",
+          en: "Especially durable fabric",
         },
       ],
     },
