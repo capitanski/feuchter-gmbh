2c8d4bc2015cbe4d865ba4924743bf716c73e438
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-04-01 09:54:22 +0200

    Implemented sanitize html for Formdata

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -2,13 +2,31 @@
 import type { APIRoute } from "astro";
 import type { M } from "docs/server/chunks/astro/server_CofuBfMf.mjs";
 import nodemailer from "nodemailer";
+import sanitizeHtml from "sanitize-html";
 
 export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData();
     const honeypot = formData.get("website");
-    const name = formData.get("name")?.toString();
-    const email = formData.get("email")?.toString();
-    const message = formData.get("message")?.toString();
+    const name = sanitizeHtml(formData.get("name")?.toString(), {
+        allowedTags: [],
+        allowedAttributes: {},
+    });
+    const email = sanitizeHtml(formData.get("email")?.toString(), {
+        allowedTags: [],
+        allowedAttributes: {},
+    });
+    const message = sanitizeHtml(formData.get("message")?.toString(), {
+        allowedTags: [],
+        allowedAttributes: {},
+    });
+    const company = sanitizeHtml(formData.get("company")?.toString(), {
+        allowedTags: [],
+        allowedAttributes: {},
+    });
+    const phone = sanitizeHtml(formData.get("tel")?.toString(), {
+        allowedTags: [],
+        allowedAttributes: {},
+    });
     if (honeypot) {
         // Wenn das Feld ausgefüllt ist, ist es wahrscheinlich ein Bot
         return new Response("Bot erkannt.", { status: 400 });
@@ -34,7 +52,10 @@ export const POST: APIRoute = async ({ request }) => {
             to: "felix@feuchter-group.de",
             subject: "Neue Kontaktanfrage",
             text: message,
-            html: `<p><strong>Von:</strong> ${name} (${email})</p><p>${message}</p>`,
+            html: `<p><strong>Von:</strong> ${name} (${email})</p><p>${message}</p>
+            <p> <strong>Firma:</strong> ${company}</p>
+            <p> <strong>Telefon:</strong> ${phone}</p>
+            `,
         });
 
         return new Response("Nachricht erfolgreich gesendet.", { status: 200 });
