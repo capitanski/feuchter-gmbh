f516b76359e84eb1b5b1afc4384f41f01d6aa554
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-05-08 13:11:51 +0200

    Bilder für Transportlösungen Kategorieseite angepasst

--- src/content/transportCategories.js
+++ src/content/transportCategories.js
@@ -4,6 +4,9 @@ import schutzwandVerschluss from "@/assets/Produkte/schutzwand_verschluss.jpg"
 import schutzwandFarbig from "@/assets/Produkte/Transportwand farbig.jpg"
 import thermohaubeFarbig from "@/assets/Staubhaube Farbig.jpg"
 import staubhaubeFarbig from "@/assets/Thermohaube farbig.jpg"
+import schutzwand from "@/assets/Produkte/schutzwand_schraeg_header.jpg"
+import thermohaube from "@/assets/Produkte/thermohaube_schraeg_header.jpg"
+import staubhaube from "@/assets/Produkte/staubhaube_schraeg_header.jpg"
 export const transportCategories = [
     {
         slug: "transportsolutions/protectivewalls",
@@ -17,7 +20,7 @@ export const transportCategories = [
 Thanks to the innovative 6-point click system and integrated fiberglass rods, the protective wall ensures maximum stability and safety – while also saving valuable time during installation.`
         },
 
-        img: schutzwandFarbig.src
+        img: schutzwand.src
     },
     {
         slug: "transportsolutions/dustcovers",
@@ -29,7 +32,7 @@ Thanks to the innovative 6-point click system and integrated fiberglass rods, th
             de: "Für besonders sensible Produkte bieten unsere Staub- und Schutzhauben optimalen Schutz vor äußeren Einflüssen wie Schmutz, Feuchtigkeit oder Beschädigung. Die hochwertigen Materialien garantieren Langlebigkeit und einfache Handhabung im täglichen Einsatz.",
             en: "For particularly sensitive products, our dust and protective covers offer optimal protection against external influences such as dirt, moisture, or damage. High-quality materials ensure long-lasting performance and easy handling in daily use."
         },
-        img: staubhaubeFarbig.src
+        img: staubhaube.src
     },
     {
         slug: "transportsolutions/thermalcovers",
@@ -41,7 +44,7 @@ Thanks to the innovative 6-point click system and integrated fiberglass rods, th
             de: "Ob heiß oder kalt – unsere Thermohauben schützen temperaturempfindliche Waren zuverlässig auf dem gesamten Transportweg. Sie sind robust, mehrfach verwendbar und sorgen für konstante Bedingungen, selbst bei wechselnden Außentemperaturen.",
             en: "Hot or cold – our thermal covers reliably protect temperature-sensitive goods throughout the entire transport process. They are durable, reusable, and ensure stable conditions even in fluctuating external temperatures."
         },
-        img: thermohaubeFarbig.src
+        img: thermohaube.src
     }
 ]
 
