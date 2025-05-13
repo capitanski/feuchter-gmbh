ef0dbee08fdc57c51b46fcb86103ecdeb6fbc1a0
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-05-13 13:03:26 +0200

    Rebuilt with correct base url in langswitcher

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CExxISlE.mjs';
-import { manifest } from './manifest_BtMm6nMm.mjs';
+import { manifest } from './manifest_Dp5-gAg5.mjs';
 
 const serverIslandMap = new Map();;
 
