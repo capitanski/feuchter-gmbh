bb90cd9737e6876796942f7a85ec798d088aa2f3
Felix Feuchter <felixzitzelsberger@yahoo.com>
2025-05-13 12:46:20 +0200

    placed start command in package.json

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CExxISlE.mjs';
-import { manifest } from './manifest_DGs9mZ1_.mjs';
+import { manifest } from './manifest__UIzRcPm.mjs';
 
 const serverIslandMap = new Map();;
 
